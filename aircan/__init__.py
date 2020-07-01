b5f35464686b4167a6a8252520f00a9296a40fd3
Shubham Mahajan <mr.shubhammahajan@gmail.com>
2020-07-01 11:26:52 +0530

    [m]: Convert library into pypi package structure

---
+++ aircan/__init__.py
@@ -0,0 +1,4 @@
+from .dependencies import api_load, hybrid_load
+from .dags import (api_ckan_load_multiple_nodes,
+                   api_ckan_load_single_node,
+                   hybrid_ckan_load)

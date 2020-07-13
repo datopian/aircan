0910f17a51ee2ecd124c90b7a6a55c4e91aff895
Shubham Mahajan <mr.shubhammahajan@gmail.com>
2020-07-13 14:59:43 +0530

    [m]: Error and Success handling #20

--- aircan/dependencies/file_conversion/csv_to_json.py
+++ aircan/dependencies/file_conversion/csv_to_json.py
@@ -1,8 +1,11 @@
 import pandas as pd
-
-
+import logging
+from aircan import NotFound
 def convert(input, output, **kwargs):
-    print('Starting file conversion')
-    df = pd.read_csv(input)
-    df.to_json(output, orient='records')
-    return {"success": True}
+    logging.info('Starting file conversion')
+    try:
+        df = pd.read_csv(input)
+        df.to_json(output, orient='records')
+        return {"success": True}
+    except FileNotFoundError as e:
+        raise NotFound(f"File {input} doesn\'t exist.")

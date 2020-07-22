5bfaaec812eb4affba6f22831525b3a7fd9e2983
michael beilin <michael.beilin@datopian.com>
2020-07-22 11:07:57 +0300

    [feature][s] add bigquery related params to DAG params

--- aircan/dags/api_ckan_import_to_bq.py
+++ aircan/dags/api_ckan_import_to_bq.py
@@ -26,7 +26,11 @@ args = {
         "schema": "['field1', 'field2']", 
         "ckan_api_key": "API_KEY",
         "ckan_site_url": "URL",
-        "output_bucket": str(date.today())
+        "output_bucket": str(date.today()),
+        "bq_project_id": "bq-project",
+        "bq_dataset_id": "bq-dataset",
+        "bq_table_name": "bq-table"
+
     }
 }
 
@@ -36,10 +40,10 @@ dag = DAG(
     schedule_interval=None
 )
 
-def task_import_resource_to_bq(gc_file_url, table_schema, project_id, dataset_id, table_name, **kwargs):
+def task_import_resource_to_bq(gc_file_url, table_schema, bq_project_id, bq_dataset_id, bq_table_name, **kwargs):
     logging.info('Importing resource to bigquery')
     # sample bq_table_id: "bigquerytest-271707.nhs_test.dag_test"
-    bq_table_id = '%s.%s.%s' % (project_id, dataset_id, table_name)
+    bq_table_id = '%s.%s.%s' % (bq_project_id, bq_dataset_id, bq_table_name)
     logging.info('Importing %s to BQ %s' % (gc_file_url, bq_table_id))
     bq_import_csv(bq_table_id, gc_file_url)
 
@@ -49,6 +53,6 @@ import_resource_to_bq_task = PythonOperator(
     task_id='import_resource_to_bq',
     provide_context=True,
     python_callable=task_import_resource_to_bq,
-    op_kwargs={'gc_file_url': "{{ params.path }}", 'table_schema': "{{ params.schema }}", 'project_id': "{{ params.project_id }}", 'dataset_id': "{{ params.dataset_id }}", 'table_name': "{{ params.table_name }}"},
+    op_kwargs={'gc_file_url': "{{ params.path }}", 'table_schema': "{{ params.schema }}", 'bq_project_id': "{{ params.bq_project_id }}", 'bq_dataset_id': "{{ params.bq_dataset_id }}", 'bq_table_name': "{{ params.bq_table_name }}"},
     dag=dag,
 )

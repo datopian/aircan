399d75dbbbd216b1ce63e74886d4a0ae1db1021a
Shubham Mahajan <mr.shubhammahajan@gmail.com>
2022-12-16 15:23:48 +0400

    [feature][composer-v2] Support v2 composer and update status to ckan

--- aircan/dependencies/google_cloud/bigquery_handler.py
+++ aircan/dependencies/google_cloud/bigquery_handler.py
@@ -1,9 +1,15 @@
 from google.cloud import bigquery
 import google.api_core.exceptions
+from aircan.dependencies.utils import AirflowCKANException, aircan_status_update
 import json
 import logging
 
-def bq_import_csv(table_id, gcs_path, table_schema):
+def replace_all(dict, string):
+    for key in dict:
+        string = string.replace(key, dict[key])
+    return string
+
+def bq_import_csv(table_id, gcs_path, table_schema, ckan_conf):
     try:
         client = bigquery.Client()
 
@@ -15,7 +21,7 @@ def bq_import_csv(table_id, gcs_path, table_schema):
         job_config.skip_leading_rows = 1
         job_config.source_format = bigquery.SourceFormat.CSV
         # overwrite a Table
-        job_config.write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE
+        job_config.write_disposition = bigquery.WriteDisposition.WRITE_TRUNCATE
         # set 'True' for schema autodetect but turning it off since we define schema in explicitly when publishing data using datapub
         # job_config.autodetect = True
         load_job = client.load_table_from_uri(
@@ -24,34 +30,66 @@ def bq_import_csv(table_id, gcs_path, table_schema):
 
         load_job.result()  # Waits for table load to complete.
         destination_table = client.get_table(table_id)
+        status_dict = {
+            'res_id': ckan_conf.get('resource_id'),
+            'state': 'progress',
+            'message': 'Data ingestion is in progress.'
+        }
+        aircan_status_update(ckan_conf.get('site_url'), ckan_conf.get('api_key'), status_dict)
         if destination_table:
+            status_dict = {
+                'res_id': ckan_conf.get('resource_id'),
+                'state': 'complete',
+                'message': "Ingession Completed"
+            }
+            aircan_status_update(ckan_conf.get('site_url'), ckan_conf.get('api_key'), status_dict)
             return {'success': True, 'message': 'BigQuery Table created successfully.'}
     except Exception as e:
-        return {'success': False, 'message': 'Failed to create BigQuery Table.'}
-    
+        replacers = {
+            'gs://dx-nhs-staging-giftless/': '',
+            'gs://dx-nhs-production-giftless/': '',
+            'gs://dx-nhs-prod-giftless/': '',
+            'https://bigquery.googleapis.com/bigquery/v2/projects/datopian-dx/jobs?prettyPrint=false': '',
+            'datopian-dx': '',
+            'bigquery': '',
+            'googleapi': '',
+            'google': ''
+
+        }
+        e = replace_all(replacers,str(e))
+        logging.info(e)
+        status_dict = {
+            'res_id': ckan_conf.get('resource_id'),
+            'state': 'failed',
+            'message': str(e)
+        }
+        aircan_status_update(ckan_conf.get('site_url'), ckan_conf.get('api_key'), status_dict)
+        raise AirflowCKANException('Data ingestion has failed.', str(e))
+
+
 def bq_schema_from_table_schema(table_schema):
     mapping = {
         'string': 'string',
         'number': 'numeric',
         'integer': 'numeric',
         'boolean': 'boolean',
-        'object':'string',
-        'array':'string',
-        'date':'date',
-        'time':'time',
+        'object': 'string',
+        'array': 'string',
+        'date': 'date',
+        'time': 'time',
         'datetime': 'datetime',
         'year': 'numeric',
         'yearmonth': 'string',
-        'duration':'datetime',
+        'duration': 'datetime',
         'geopoint': 'string',
-        'geojson' : 'string',
+        'geojson': 'string',
         'any': 'string'
-      }
+    }
 
     def _convert(field):
-        # TODO: support for e.g. required
+        # Â TODO: support for e.g. required
         return bigquery.SchemaField(field['name'],
-            mapping.get(field['type'], field['type']),
-            'NULLABLE'
-            )
-    return [ _convert(field) for field in table_schema ]
+                                    mapping.get(field['type'], field['type']),
+                                    'NULLABLE'
+                                    )
+    return [_convert(field) for field in table_schema]
